fixed bug in backpropagation, implemented SGD optimizer

NeuralNetwork.py
@@ -12,7 +12,6 @@ class Layer:
         self.output = np.dot(inputs, self.weights) + self.biases
 
     def backward(self, next_layer_dvalues):
-        raise NotImplementedError
         self.dinputs = np.dot(next_layer_dvalues, self.weights.T)
         self.dweights = np.dot(self.inputs.T, next_layer_dvalues)
         self.dbiases = np.sum(next_layer_dvalues, axis = 0, keepdims = True)
@@ -67,6 +66,11 @@ class NeuralNetwork:
         output_layer = Layer(self.layer_width, self.n_outputs)
         self.layers.append(output_layer)
 
+        self.activations = []
+        for i in range(self.n_hidden_layers):
+            self.activations.append(ActivationReLU())
+        self.activations.append(ActivationSoftmax())
+
     def random_perturbation(self):
 
         for i in range(len(self.layers)):
@@ -77,21 +81,53 @@ class NeuralNetwork:
 
         self.inputs = inputs
 
-        activation_relu = ActivationReLU()
         partial_output = inputs
-        for i in range(len(self.layers) - 1):
+        for i in range(len(self.layers)):
             self.layers[i].forward(partial_output)
             partial_output = self.layers[i].output
-            activation_relu.forward(partial_output)
-            partial_output = activation_relu.output
+            self.activations[i].forward(partial_output)
+            partial_output = self.activations[i].output
 
-        activation_softmax = ActivationSoftmax()
-        self.layers[-1].forward(partial_output)
-        partial_output = self.layers[-1].output
-        activation_softmax.forward(partial_output)
-        final_output = activation_softmax.output
+        self.output = partial_output
 
-        self.output = final_output
+    def backward(self, next_layer_dvalues):
+
+        dvalues = next_layer_dvalues
+        for i in range(len(self.layers) - 1, -1, -1):
+            self.activations[i].backward(dvalues)
+            dvalues = self.activations[i].dinputs
+            self.layers[i].backward(dvalues)
+            dvalues = self.layers[i].dinputs
+
+    def train(self, data, true_labels, n_iterations):
+            
+            accuracy_function = Accuracy()
+            loss_function = LossCategoricalCrossentropy()
+            self.forward(data)
+            loss = loss_function.calculate(self.output, true_labels)
+            print("loss: ", loss)
+
+            optimizer = Optimizer_SGD(decay=1e-3)
+
+            loss_function.backward(self.output, true_labels)
+            dvalues = loss_function.dinputs
+            for i in range(n_iterations):
+                
+                self.backward(dvalues)
+                
+                optimizer.update_learning_rate()
+                optimizer.update_network_params(self)
+                optimizer.update_iteration()
+
+                self.forward(data)
+                loss = loss_function.calculate(self.output, true_labels)
+                accuracy = accuracy_function.calculate(self.output, true_labels)
+                if i % 100 == 0:
+                    print(f"iteration: {i}, learning rate: {optimizer.current_learning_rate}, loss: {loss}, accuracy: {accuracy}.")
+
+                loss_function.backward(self.output, true_labels)
+                dvalues = loss_function.dinputs
+                
 
     def random_train(self, data, true_labels, n_iterations):
 
@@ -158,4 +194,29 @@ class LossCategoricalCrossentropy(Loss):
         self.dinputs = - y_true / next_layer_dvalues
         
         # Normalize gradient
-        self.dinputs = self.dinputs / samples
+        self.dinputs = self.dinputs / number_samples
+
+class Optimizer_SGD :
+    
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
+    def __init__ (self, learning_rate: float = 1.0, decay: float = 0.0):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.current_iteration = 0
+    
+    def update_learning_rate(self):
+        self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.current_iteration))
+
+    # Update parameters
+    def update_layer_params (self, layer: Layer ):
+        layer.weights += - self.learning_rate * layer.dweights
+        layer.biases += - self.learning_rate * layer.dbiases
+
+    def update_iteration(self):
+        self.current_iteration += 1
+
+    def update_network_params(self, network: NeuralNetwork):
+        for layer in network.layers:
+            self.update_layer_params(layer)

main.py
@@ -10,7 +10,7 @@ X, y = spiral_data(samples=100, classes=3)
 plt.scatter(X[:, 0 ], X[:, 1 ], c = y, s = 40 , cmap = 'brg' )
 plt.show()
 
-neural_network = NeuralNetwork(2, 1, 3, 3)
+neural_network = NeuralNetwork(2, 1, 64, 3)
 
 neural_network.forward(X)
 output = neural_network.output
@@ -27,7 +27,7 @@ predictions = np.argmax(output, axis = 1)
 plt.scatter(X[:, 0 ], X[:, 1 ], c = predictions, s = 40 , cmap = 'brg' )
 plt.show()
 
-neural_network.random_train(X, y, 1000)
+neural_network.train(X, y, 10000)
 neural_network.forward(X)
 output = neural_network.output
 